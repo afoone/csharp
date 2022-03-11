e176837ef9fd07f6142ba6b80989db481cd33785
Alfonso Tienda <afoone@hotmail.com>
2022-03-11 22:20:30 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace hola_mundo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //string nombre;
+            //Console.Write("Ingresa tu nombre:  ");
+            //nombre = Console.ReadLine();
+            //Console.WriteLine("Hola {0}!", nombre);
+
+            //ushort a, b;
+            ////string valor;
+            //Console.Write("Ingresa el primer número natural : ");
+            //string valor = Console.ReadLine();
+            //a = Convert.ToUInt16(valor);
+
+            //Console.Write("Ingresa el segundo número natural : ");
+            //valor = Console.ReadLine();
+            //b = ushort.Parse(valor);
+
+            //int resultado = a + b;
+
+            //Console.WriteLine("{0} + {1} = {2}", a, b, resultado);
+
+
+
+            //Console.Title="Area del triángulo";
+            //ushort b, h;
+            //Console.Write("Introduce la base: ");
+            //string baseValue = Console.ReadLine();
+            //b = Convert.ToUInt16(baseValue);
+            //Console.Write("Intrucide la altura:  ");
+            //h = Convert.ToUInt16(Console.ReadLine());
+            //Console.Beep();
+            //Console.WriteLine($"El area es {b * h / 2}");
+            //Console.WriteLine($"Y PI es {Math.PI:0.00}");
+
+            //void Count(int max)
+            //{
+            //    for(int i = 1; i<= max; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Count(17);
+
+            //ushort readNumber(string name)
+            //{
+            //    Console.Write($"Introduce {name}: ");
+            //    string input = Console.ReadLine();
+            //    return Convert.ToUInt16(input);
+            //}
+
+            //ushort b, h;
+            //b = readNumber("base");
+            //h = readNumber("altura");
+            //Console.WriteLine($"El area es {b * h / 2}");
+
+            string response = "";
+            Chest chest = Chest.Locked;
+
+            while (response != "exit")
+            {
+
+
+
+            }
+        
+
+          
+
+            enum Chest { Open, Closed, Locked }; // los nuevo tipos tienen que ir detrás de código o en otro fichero
+
+
+        }
+    }
+}
