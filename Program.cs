326ab309efa0d9f9ee5465c119b9f256f0e4bc6d
Alfonso Tienda <afoone@hotmail.com>
2022-03-11 22:29:47 +0100

    ejercicio enumeraciones

--- Program.cs
+++ Program.cs
@@ -6,76 +6,60 @@ namespace hola_mundo
     {
         public static void Main(string[] args)
         {
-            //string nombre;
-            //Console.Write("Ingresa tu nombre:  ");
-            //nombre = Console.ReadLine();
-            //Console.WriteLine("Hola {0}!", nombre);
-
-            //ushort a, b;
-            ////string valor;
-            //Console.Write("Ingresa el primer número natural : ");
-            //string valor = Console.ReadLine();
-            //a = Convert.ToUInt16(valor);
-
-            //Console.Write("Ingresa el segundo número natural : ");
-            //valor = Console.ReadLine();
-            //b = ushort.Parse(valor);
-
-            //int resultado = a + b;
-
-            //Console.WriteLine("{0} + {1} = {2}", a, b, resultado);
-
-
-
-            //Console.Title="Area del triángulo";
-            //ushort b, h;
-            //Console.Write("Introduce la base: ");
-            //string baseValue = Console.ReadLine();
-            //b = Convert.ToUInt16(baseValue);
-            //Console.Write("Intrucide la altura:  ");
-            //h = Convert.ToUInt16(Console.ReadLine());
-            //Console.Beep();
-            //Console.WriteLine($"El area es {b * h / 2}");
-            //Console.WriteLine($"Y PI es {Math.PI:0.00}");
-
-            //void Count(int max)
-            //{
-            //    for(int i = 1; i<= max; i++)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
-
-            //Count(17);
-
-            //ushort readNumber(string name)
-            //{
-            //    Console.Write($"Introduce {name}: ");
-            //    string input = Console.ReadLine();
-            //    return Convert.ToUInt16(input);
-            //}
-
-            //ushort b, h;
-            //b = readNumber("base");
-            //h = readNumber("altura");
-            //Console.WriteLine($"El area es {b * h / 2}");
-
             string response = "";
             Chest chest = Chest.Locked;
 
             while (response != "exit")
             {
+                string state = "";
 
+                switch (chest)
+                {
+                    case Chest.Locked:
+                        state = "cerrado con llave";
+                        break;
+                    case Chest.Closed:
+                        state = "cerrado";
+                        break;
+                    case Chest.Open:
+                        state = "abierto";
+                        break;
+                }
+
+                Console.Write($"Está {state}, ¿Qué haces?");
+                response = Console.ReadLine();
+
+                switch (response)
+                {
+                    case "unlock":
+                        if (chest == Chest.Locked)
+                            chest = Chest.Closed;
+                        break;
+                    case "open":
+                        if (chest == Chest.Closed)
+                            chest = Chest.Open;
+                        break;
+                    case "close":
+                        if (chest == Chest.Open)
+                            chest = Chest.Closed;
+                        break;
+                    case "lock":
+                        if (chest == Chest.Closed)
+                            chest = Chest.Locked;
+                        break;
+                }
 
 
             }
+
+            Console.WriteLine("juego finalizado");
         
 
           
 
-            enum Chest { Open, Closed, Locked }; // los nuevo tipos tienen que ir detrás de código o en otro fichero
-
-
         }
+
+        enum Chest { Open, Closed, Locked }; // los nuevo tipos tienen que ir detrás de código o en otro fichero
+
     }
 }
